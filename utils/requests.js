6b444acb724790951209620bb0d9cfb03603e9d5
MhMahdi94 <mohdmahdy94@gmail.com>
2024-03-18 15:41:36 +0400

    add property page

--- utils/requests.js
+++ utils/requests.js
@@ -1,20 +1,44 @@
-const apiDomain = process.env.NEXT_PUPLIC_API_DOMAIN || null;
+const apiDomain ='http://localhost:3000/api';// process.env.NEXT_PUPLIC_API_DOMAIN || null;
 
-async function fetchProerties(){
-    try {
-        if(!apiDomain){
-            return [];
-        }
-      const res = await fetch(`${apiDomain}/properties`)
-      if(!res.ok){
-        throw new Error('Faild to fetch data');
-      }
-  
-      return res.json();
-    } catch (error) {
-      console.log(error);
+//fetch properties
+async function fetchProerties() {
+ 
+  try {
+    if (!apiDomain) {
       return [];
     }
-  }
+    const res = await fetch(`${apiDomain}/properties`);
+    if (!res.ok) {
+      throw new Error("Faild to fetch data");
+    }
 
-  export {fetchProerties};
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+//fetch property
+async function fetchProerty(id) {
+  console.log(apiDomain);
+  try {
+    if (!apiDomain) {
+        console.log(1)
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+    if (!res.ok) {
+        console.log(2)
+      throw new Error("Faild to fetch data");
+    }
+    console.log(3)
+    return res.json();
+  } catch (error) {
+    console.log(4)
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProerties, fetchProerty };
